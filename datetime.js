4fa49a4e66248ed4021ba0ac2911cc7cd54798b9
brucechen <brucechenofnju@gmail.com>
2012-12-12 20:17:22 +0800

    add program comments
    Signed-off-by: brucechen <brucechenofnju@gmail.com>

--- datetime.js
+++ datetime.js
@@ -1,29 +1,36 @@
 var DateTime = (function(Date,Math,undefined){
 
 	var proto = DateTime.prototype;
-	/** methods for get set
-	 **/
+	
+	/** 
+	 * methods for get set
+	**/
 	var MethodNames = [
-	'FullYear',     // 0
-	'Month',        // 1
-	'Date',         // 2
-	'Hours',        // 3
-	'Minutes',      // 4
-	'Seconds',      // 5
-	'Milliseconds', // 6
-	'Day',          // 7
-	'Year',         // 8
-	'Time',
-	'TimezoneOffset',
-	'UTCDate',
-	'UTCDay',
-	'UTCMonth',
-	'UTCFullYear',
-	'UTCHours',
-	'UTCMinutes',
-	'UTCSeconds',
-	'UTCMilliseconds'
-	]
+
+		'FullYear',     
+		'Month',       
+		'Date',        
+		'Hours',        
+		'Minutes',      
+		'Seconds',      
+		'Milliseconds', 
+		'Day',          
+		'Year',         
+		'Time',
+		'TimezoneOffset',
+		'UTCDate',
+		'UTCDay',
+		'UTCMonth',
+		'UTCFullYear',
+		'UTCHours',
+		'UTCMinutes',
+		'UTCSeconds',
+		'UTCMilliseconds'
+	];
+
+	/**
+	 * toString methods
+	**/
 	var StringMethodNams = [
 		'TimeString',
 		'GMTString',
@@ -32,7 +39,11 @@ var DateTime = (function(Date,Math,undefined){
 		'LocaleTimeString',
 		'LocaleDateString',
 		'DateString'
-	]
+	];
+
+	/**
+	 * time string formatter
+	 **/
 	var Format = {
 		'Year':/y{4}/g,
 		'Short_Year':/y{2}/g,
@@ -42,12 +53,16 @@ var DateTime = (function(Date,Math,undefined){
 		'Minute':/mm/g,
 		'Second':/ss/g,
 		'Millisecond':/ms/g
-	}
+	};
 
 	function DateTime(){
+
 		return this._init(arguments);
 	}
-	
+
+	/**
+	 * @private
+	**/
 	DateTime.prototype._init = function(args){
 		var length = args.length;
 		if(length===0){
@@ -61,11 +76,13 @@ var DateTime = (function(Date,Math,undefined){
 				this._date = new Date(args[0]);
 			}
 		}else if(length === 3){
+			
 			var year = args[0];
 			var month = args[1];
 			var day = args[2];
 			this._date = new Date(year,month,day);
 		}else if(length === 6){
+
 			var year = args[0];
 			var month = args[1];
 			var day = args[2];
@@ -77,49 +94,81 @@ var DateTime = (function(Date,Math,undefined){
 		return this;
 	}
 
+	/**
+	 * clone get and set methods of Date
+	**/
 	cloneMethod(MethodNames,function(name){
 		proto['get'+ name] = function(){
 			return this._date['get' + name]();
 		}
-		// need clone set method
 		proto['set' + name] = function(value){
 			return this._date['set'+ name](value);
 		}
 	});
+
+	/**
+	 * clonet to*String methods of Date
+	**/
 	cloneMethod(StringMethodNams,function(name){
 		proto['to' + name] = function(){
 			return this._date['to' + name]();
 		}
 	});
+
 	/**
-	 * DateTime methods
-	 **/
-	 proto.toString = function(formatter){
+	 * @public 
+	 * convert DateTime to string with a given formater. 
+	 * yyyy and yy means year , MM means month,dd means day
+	 * HH means hour, mm means minutes, ss means seconds and ms means milliseconds.
+	**/
+	proto.toString = function(formatter){
+
 	 	if(formatter === undefined){
+
 	 		return this._date.toString();
 	 	}
+
 	 	if(Format.Year.test(formatter)){
+
 	 		formatter = formatter.replace(Format.Year,this._date.getUTCFullYear()+"");
+
 	 	}else if(Format.Short_Year.test(formatter)){
+
 	 		formatter = formatter.replace(Format.Short_Year,(this._date.getUTCFullYear()+"").substr(2,2));
 	 	}
+
 	 	var monstr = "";
+
 	 	if(this._date.getUTCMonth() + 1< 10){
+
 	 		monstr = "0" + (this._date.getUTCMonth() + 1);
+
 	 	}else{
+
 	 		monstr = '' + this._date.getUtcDate();
 	 	}
+
 	 	var daystr = '' + this._date.getUtcDate();
+
 	 	if(this._date.getUTCDate() < 10){
+
 	 		daystr = '0' + daystr;
 	 	}
+
 	 	var hourstr = '';
+
 	 	if(this._date.getUTCHours()<10){
+
 	 		hourstr = '0' + this._date.getUTCHours();
+
 	 	}else{
+
 	 		hourstr = "" + this._date.getUTCHours();
+
 	 	}
+
 	 	var minstr = '' + this._date.getUTCMinutes();
+
 	 	if(this._date.getUTCMinutes < 10){
 	 		minstr = '0' + minstr;
 	 	}
@@ -138,51 +187,81 @@ var DateTime = (function(Date,Math,undefined){
 	 					.replace(Format.Second,seconstr)
 	 					.replace(Format.Millisecond,milistr);
 
-	 }
-	 proto.parse = function(datestr,formatter){
+	}
+
+	/**
+	 * @public
+	 * parse DateTime string
+	**/
+	proto.parse = function(datestr,formatter){
+
 	 	if(typeof datestr == DateTime || typeof datestr == Date){
+
 	 		return new DateTime(datestr);
 	 	}
+
 	 	if(typeof formatter === undefined){
+
 	 		return Date.parse(datestr);
 	 	}
 
 	 	var year,month,day,hour,minute,second,millisecond;
+
 	 	var result;
+
 	 	if((result = Format.Year.exec(formatter)) !== null){
+
 	 		year = datestr.substr(result.index,result[0].length)*1;
+
 	 	}else if ((result = Format.Short_Year.exec(formatter)) !== null) {
+
 	 		year = datestr.substr(result.index,result[0].length)*1 +2000;
+
 	 	}else{
 	 		return null;
 	 	}
 
 	 	if ((result = Format.Month.exec(formatter)) !== null) {
+
 	 		month = datestr.substr(result.index,result[0].length)*1;
+
 	 	}else{
+
 	 		return null;
 	 	}
 
 	 	if ((result = Format.Day.exec(formatter)) !== null){
+
 	 		day = datestr.substr(result.index,result[0].length)*1;
+
 	 	} else{
+
 	 		return null;
 	 	}
 	 	if ((result = Format.Hour.exec(formatter)) !== null){
+
 	 		hour = datestr.substr(result.index,result[0].length)*1;
+
 	 	} else{
+
 	 		hour = 0;
 	 	}
 
 	 	if ((result = Format.Minute.exec(formatter)) !== null){
+
 	 		minute = datestr.substr(result.index,result[0].length)*1;
+
 	 	} else{
+
 	 		minute = 0;
 	 	}
 
 	 	if ((result = Format.Second.exec(formatter)) !== null){
+
 	 		second = datestr.substr(result.index,result[0].length)*1;
+
 	 	} else{
+
 	 		second = 0;
 	 	}
 
@@ -190,118 +269,240 @@ var DateTime = (function(Date,Math,undefined){
 
 	 }
 
-	 proto.addYears = function(delta){
+	/**
+	 * @public 
+	 * add delta years to datetime
+	**/
+	proto.addYears = function(delta){
 
 	 	return this.addMonths(value * 12);
+	}
 
-	 }
-	 proto.addMonths = function(delta){
+	/**
+	 * @public 
+	 * add delta months to datetime
+	**/
+	proto.addMonths = function(delta){
         var n = this.getDate();
         this.setDate(1);
         this.setMonth(this.getMonth() + delta * 1);
         this.setDate(Math.min(n, DateTime.getDaysInMonth(this.getFullYear(), this.getMonth())));
         return this;
-	 }
-	 proto.addDays = function(delta){
+	}
+
+	/** 
+	 * @public 
+	 * add delta days to datetime
+	**/
+	proto.addDays = function(delta){
 	 	this.setDate(this.getDate() + value * 1);
 	 	return this;
-	 }
-	 proto.addHours = function(delta){
-	 	return this.addMilliseconds(value * 3600000);
-	 }
-	 proto.addMinutes = function(delta){
+	}
+
+	/**
+	 * @public 
+	 * add delta hours to datetime
+	**/
+	proto.addHours = function(delta){
+
+		return this.addMilliseconds(value * 3600000);
+	}
+
+	/**
+	 * @public 
+	 * add delta minutes to datetime 
+	**/
+	proto.addMinutes = function(delta){
+	 	
 	 	return this.addMilliseconds(value * 60000); /* 60*1000 */
-	 }
-	 proto.addSeconds = function(delta){
-	 	return this.addMilliseconds(value * 1000); 
-	 }
-	 proto.addMilliseconds = function(delta){
+	}
+
+	/**
+	 * @public 
+	 * add delta seconds to datetime
+	**/
+	proto.addSeconds = function(delta){
+
+		return this.addMilliseconds(value * 1000); 
+	}
+
+	/**
+	 * @public 
+	 * add delta milliseconds to datetime
+	**/
+	proto.addMilliseconds = function(delta){
+	
 	 	this.setMilliseconds(this.getMilliseconds() + delta * 1);
 	 	return this;
-	 }
-	 proto.spanYears = function(dateTime){
+	}
+	
+	/**
+	 * @public 
+	 * diff years with datetime
+	**/
+	proto.spanYears = function(dateTime){
+	
 	 	return this.getFullYear() - datetime.getFullYear();
-	 }
-	 proto.spanMonths = function(dateTime){
+	}
+	
+	/**
+	 * @public 
+	 * diff months with datetime
+	**/
+	proto.spanMonths = function(dateTime){
+	
 	 	return this.getMonth() - dateTime.getMonth();
-	 }
-	 proto.spanDays = function(dateTime){
+	}
+
+	/**
+	 * @public 
+	 * diff days with datetime
+	**/
+	proto.spanDays = function(dateTime){
+	
 	 	return this.getDate() - dateTime.getDate();
+	}
 
-	 }
-	 proto.spanHours = function(dateTime){
+	/**
+	 * @public 
+	 * diff hours with datetime
+	**/
+	proto.spanHours = function(dateTime){
+
 	 	return this.getHours() - datetime.getHours();
+	}
 
-	 }
-	 proto.spanMinutes = function(dateTime){
+	/**
+	 * @public 
+	 * diff minutes with datetime
+	**/
+	proto.spanMinutes = function(dateTime){
+
 	 	return this.getMinutes() - datetime.getMinutes();
+	}
+	
+	/**
+	 * @public 
+	 * diff seconds with datetime
+	**/
+	proto.spanSeconds = function(dateTime){
 
-	 }
-	 proto.spanSeconds = function(dateTime){
 	 	return this.getSeconds() - datetime.getSeconds();
+	}
+	
+	/**
+	 * @public 
+	 * diff milliseconds with datetime
+	**/	
+	proto.spanMilliseconds = function(dateTime){
 
-	 }
-	 proto.spanMilliseconds = function(dateTime){
 	 	return this.getMilliseconds() - dateTime.getMilliseconds();
+	}
 
-	 }
-	 proto.toDate = function(){
+	/**
+	 * @public 
+	 * convert DateTime object to DateTime
+	**/
+	proto.toDate = function(){
 	 	return this._date;
 	 }
-	 proto.compareTo = function(dateTime){
+
+	/**
+	 * @public 
+	 * compare with datetime;
+	 * @return -1 while it is earlier than datetime
+	 * @return 0 while it is same with datetime
+	 * @return 1 while it is later than dateime
+	**/
+	proto.compareTo = function(dateTime){
 	 	if(isNan(dateTime)){
 	 		throw new Error(dateTime);
 	 	}
+
 	 	if(dateTime instanceof DateTime){
 	 		return (this._date < dateTime.toDate()) ? -1 : (this._date > dateTime.toDate()) ? 1 : 0;
 	 	}
+	}
 
-	 }
-	 proto.clone = function(){
+	/**
+	 * @public 
+	 * clone method
+	**/
+	proto.clone = function(){
+	
 	 	return new DateTime(this._date.getTime());
-	 }
-	 proto.validDate = function(dateTime){
+	}
+
+	/**
+	 * @public 
+	 * validate datetime
+	**/
+	proto.validDate = function(dateTime){
 	 	if(typeof dateTime === undefined){
 	 		dateTime = this;
 	 	}
 	 	return !isNaN(+dateTime._date);
-	 }
-	 /**
-	  * static method 
-	  **/
-	 DateTime.today = function(){
+	}
+
+	/**
+	 * static method 
+	 * today for example 2012-12-12 00:00:00
+	**/
+	DateTime.today = function(){
 	 	DateTime dt = DateTime.now();
 	 	dt.setHours(0)
 	 	  .setMinutes(0)
 	 	  .setSeconds(0)
 	 	  .setMilliseconds(0);
 	 	return dt;
-	 }
-	 DateTime.now = function(){
+	}
+
+	/**
+	 * @public 
+	 * static method
+	 * @return now ,for example 2012-12-12 17:04:04
+	**/
+	DateTime.now = function(){
+	
 	 	DateTime dt = new DateTime();
 	 	return dt;
-	 }
-	 DateTime.isLeapYear = function(year){
-	 	return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0); 
-	 }
-	 DateTime.getDaysInMonth = function(year,month){
-	 	return [31, (DateTime.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
-	 }
+	}
+
 	/**
-	 * clone get and set methods from Date
-	 **/
-	 function cloneMethod(names,fun){
+	 * @public 
+	 * static method
+	 * @return true if year is leap year
+	**/
+	DateTime.isLeapYear = function(year){
+	 	
+	 	return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0); 
+	}
+
+	/**
+	 * @public 
+	 * static method
+	 * get how many days in month 
+	**/
+	DateTime.getDaysInMonth = function(year,month){
+	
+	 	return [31, (DateTime.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+	}
+
+	/***********************************    Utilities    ************************************************/
+	
+	function cloneMethod(names,fun){
 	 	for(var i =0;i<names.length;i++){
 	 		fun(names[i],i);
 	 	}
-	 }
+	}
 
 	function isNumber(arg){
 		return typeof arg === 'number';
 	}
+	
 	function isString(arg){
 		return typeof arg === "string";
 	}
 	
-	return DateTime;		
+	return DateTime;	
 })(Date,Math)
